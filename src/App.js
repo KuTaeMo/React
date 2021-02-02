ef0f919502908ed275d570ae9c58d8a089886844
KuTaeMo <67215505+KuTaeMo@users.noreply.github.com>
2021-02-03 01:27:17 +0900

    React 기초 배우는 중 - 페이지 구조

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// useRef (디자인)
+// dom을 변경할 때 사용
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
